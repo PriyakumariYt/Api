c0442c571467ce49c0dc93c6aec91c0ec0f50d72
Priya Kumari <127418517+PriyakumariYt@users.noreply.github.com>
2023-05-22 11:02:10 +0530

    upload

---
+++ ApiGenrate/index.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let products = document.querySelector(".products");
+
+  async function fetchProduct(url) {
+    try {
+      let data = await fetch(url);
+      let response = await data.json();
+      console.log(response);
+      for (let i = 0; i < response.length; i++) {
+        let description = response[i].description;
+        let title = response[i].title;
+
+        products.innerHTML += `
+							<div class="product">
+								<img src="${response[i].image}" alt="${
+          response[i].category
+        }" class= product-img />
+         <div class="product-content">
+								 <h2 class = "product-title">${
+                   title.length > 18
+                     ? title.substring(0, 18).concat("...more")
+                     : title
+                 }</h2>
+							 <p class = "product-description">${
+                 description.length > 80
+                   ? description.substring(0, 80).concat("...more")
+                   : description
+               }</p>
+								 <div class="product-price-container">
+         <h3 class = "product-price">$${response[i].price}</h3>
+             <a href="#!" data-productId="${
+               response[i].id
+             }" class="add-to-cart"><i class="fa-solid fa-cart-shopping"></i></a>
+            </div>
+							</div>
+                            </div>
+						`;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  fetchProduct("https://fakestoreapi.com/products");
+});
+
+// 	<div class="product">
+// 		<img src="${response[i].image}" alt="${response[i].category}" />
+// 		<h2>${title}</h2>
+// 		<p>${description}</p>
+// 		<h3>$${response[i].price}</h3>
+// 	</div>
+// `;
